4fbd46a77dbf7357b4ff1988d3cfe079499a5064
Sangkeun Kim <sisobus1@gmail.com>
2014-10-23 19:18:03 +0900

    complete

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 NaverBookCrawler
 ================
 
-for HSB
--- update.sh
+++ update.sh
@@ -4,3 +4,4 @@ python parse.py
 echo "parse complete"
 python update_db.py
 echo "db update complete"
+cp info.sqlite /home/hsb/public_html
